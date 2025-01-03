25e8f9d735680d3d66e0fa159cfa54288303e238
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2025-01-03 15:30:47 +0530

    Updated

--- Server/Models/User.js
+++ Server/Models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
   userName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: flase}
+  role: { type: String, required: true}
 });
 
 UserSchema.pre('save', async function (next) {
